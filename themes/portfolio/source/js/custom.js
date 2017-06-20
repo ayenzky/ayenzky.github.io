cdaa9dc55d18ff5ecf9e33811872b1a81277d72c
ayenzky <allen.support@webfactories.biz>
2017-06-20 16:46:51 +0800

    add custom js

---
+++ themes/portfolio/source/js/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#contact-form").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
+  });
+});
+})
